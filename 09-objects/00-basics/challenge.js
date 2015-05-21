908fdbfe1ac41ed8f3301405aa2ca419ea6263f7
Patrick Mathewson <sacque@yahoo.com>
2015-05-21 13:49:13 +1200

    09-objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function (courseTitle, courseDuration, courseStudents) {
+  var course = {};
+  course.title = courseTitle;
+  course.duration = courseDuration;
+  course.students = courseStudents;
 
-module.exports.addProperty = undefined;
+  return course;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function (obj, newProp, newValue) {
+  if (!(newProp in obj)) {
+    obj[newProp] = newValue;
+  }
+  
+  return obj;
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function (letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\n" + "Sincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function (item, money) {
+  var priceTable = {};
+  priceTable['MacBook Air'] = 999;
+  priceTable['MacBook Pro'] = 1299;
+  priceTable['Mac Pro'] = 2499;
+  priceTable['Apple Sticker'] = 1;
+
+  return priceTable[item] <= money;
+};
